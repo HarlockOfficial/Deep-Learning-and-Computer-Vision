6f82d236c71cccda052ecd7515ef03c93b99f6bb
HarlockOfficial <harlockofficial.github@gmail.com>
2023-07-07 14:43:15 +0200

    [fix] minor fix NOTE: add missing new lines at end of files and add missing parameter to function

--- training/graph_convolutional_network.py
+++ training/graph_convolutional_network.py
@@ -7,5 +7,5 @@ def train_graph_convolutional_network(n_output_labels, x_train, y_train):
     return utility.train_network(GCN, n_output_labels, x_train, y_train)
 
 
-def test_graph_convolutional_network(x_test, y_test):
-    return utility.test_network(GCN, x_test, y_test)
+def test_graph_convolutional_network(n_output_labels, x_test, y_test):
+    return utility.test_network(GCN, n_output_labels, x_test, y_test)
